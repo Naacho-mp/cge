bfa306ec50ac3e3f01cffae67d7bbf750ee638b5
B-oviedo <bnjmnvd@gmail.com>
2025-11-07 17:14:01 -0300

    Se realizo el contructor de clientes y formulario listo

--- front_cge/src/app/components/cliente-data/cliente-data.ts
+++ front_cge/src/app/components/cliente-data/cliente-data.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule, FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-cliente-data',
-  imports: [FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cliente-data.html',
   styleUrl: './cliente-data.css',
+  standalone: true
 })
 export class ClienteData {
+
+  form: FormGroup;
+
+  estados =['Activo', 'Inactivo'];
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.form = this.formBuilder.group({
+      rut: [''],
+      nombre_razon: [''],
+      email_contacto: [''],
+      telefono: [''],
+      direccion_facturacion: [''],
+      estado: [''],
+    });
+  }
+
 
 }
